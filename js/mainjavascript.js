5437de95c024f359d33ed92aedbaac4c9e96606d
simone menichetti <simon3menichettui95@gmail.com>
2024-06-04 15:40:54 +0200

    inserimento constante sconto minorenni ed inizio applicazione sconti

--- js/mainjavascript.js
+++ js/mainjavascript.js
@@ -1,3 +1,5 @@
+
+
 // chiedere all’utente il numero di chilometri che vuole percorrere
 
 let kmUtente = parseInt(prompt("inserisci il kilometraggio da percorrere"));
@@ -7,19 +9,33 @@ console.log("il km scelto dall'utente è:",kmUtente);
 
 // chiedere l’età del passeggero.
 
-let ageUtende = parseInt(prompt("inserisci la tua età"));
+let ageUtente = parseInt(prompt("inserisci la tua età"));
 
-console.log("l'età dell'utente è:", ageUtende);
+console.log("l'età dell'utente è:", ageUtente);
 
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const prezzoKm = 0.21 * kmUtente;
-console.log("il prezzo al km è: ", prezzoKm);
+const  prezzoKm = 0.21;
 
+ let risultatoPrezzoKm = prezzoKm * kmUtente;
+
+console.log("il risultato del prezzo viaggio tot è: ", risultatoPrezzoKm);
 
 
 // va applicato uno sconto del 20% per i minorenni
+const scontoMinorenni = 0.20 
+
+if (ageUtente < 18) {
+  
+    // let scontoFinaleMinorenni = 
+}
+
+// } else (ageUtente >= 18 && ageUtente < 65 ) {
+//     sconto= "lo sconto non viene applicato";
+// }
+
+
 
 
 // va applicato uno sconto del 40% per gli over 65.
